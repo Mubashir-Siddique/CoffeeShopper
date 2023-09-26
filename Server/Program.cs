9cbff5663dd5a885b4528473d1f7acd5eadbbc52
Mubashir-Siddique <mubashir.siddique.ubit@gmail.com>
2023-09-27 00:35:10 +0500

    Migrations & Libraries-IdentityServer

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddIdentityServer()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+        b.UseSqlServer(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+app.UseIdentityServer();
 app.Run();
