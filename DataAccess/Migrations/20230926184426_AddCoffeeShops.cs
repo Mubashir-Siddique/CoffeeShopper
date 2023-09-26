6905ea7a283ce55f39d621e9645521e8623949f9
Mubashir-Siddique <mubashir.siddique.ubit@gmail.com>
2023-09-26 23:56:12 +0500

    CoffeeService added

---
+++ DataAccess/Migrations/20230926184426_AddCoffeeShops.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class AddCoffeeShops : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($"INSERT INTO [dbo].[CoffeeShops]([Name] ,[OpeningHours],[Address])     VALUES('Nescafe' ,'9-5 Mon-Sat','Test Address')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[CoffeeShops]([Name] ,[OpeningHours],[Address])     VALUES('Nescafe' ,'9-5 Mon-Sat','Test Address')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[CoffeeShops]([Name] ,[OpeningHours],[Address])     VALUES('Nescafe' ,'9-5 Mon-Sat','Test Address')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[CoffeeShops]([Name] ,[OpeningHours],[Address])     VALUES('Nescafe' ,'9-5 Mon-Sat','Test Address')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[CoffeeShops]([Name] ,[OpeningHours],[Address])     VALUES('Nescafe' ,'9-5 Mon-Sat','Test Address')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[CoffeeShops]([Name] ,[OpeningHours],[Address])     VALUES('Nescafe' ,'9-5 Mon-Sat','Test Address')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[CoffeeShops]([Name] ,[OpeningHours],[Address])     VALUES('Nescafe' ,'9-5 Mon-Sat','Test Address')");
+            migrationBuilder.Sql($"INSERT INTO [dbo].[CoffeeShops]([Name] ,[OpeningHours],[Address])     VALUES('Nescafe' ,'9-5 Mon-Sat','Test Address')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
